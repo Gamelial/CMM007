c14087f4f7a64f7bdb4ee1de6c1f37e8cea21c6e
Gamelial <chukwudiokere247@gmail.com>
2023-02-28 22:29:49 +0000

    new

---
+++ assets/components/Admin/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="h-14 bg-white mt-10 flex items-center justify-center">
+      <h4 className="text-xl font-poppins font-black">
+        built with love by Gamelial
+      </h4>
+    </div>
+  );
+};
+
+export default Footer;
