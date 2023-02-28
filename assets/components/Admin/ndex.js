17895620627515a684ebf01ef2bdb75f949e5741
Gamelial <chukwudiokere247@gmail.com>
2023-02-28 22:34:45 +0000

    new

---
+++ assets/components/Admin/ndex.js
@@ -0,0 +1,12 @@
+export { default as TopBar } from "./TopBar";
+export { default as Stories } from "./Stories";
+export { default as CustomSelect } from "./CustomSelect";
+export { default as StoryList } from "./StoryList";
+export { default as Story } from "./Story";
+export { default as WriteStory } from "./WriteStory";
+export { default as Footer } from "./Footer";
+export { default as AdminStories } from "./admin/AdminStories";
+export { default as AdminStory } from "./admin/AdminStory";
+export { default as AdminReaderList } from "./admin/AdminReaderList";
+export { default as AdminWriterList } from "./admin/AdminWriterList";
+export { default as AdminNav } from "./admin/AdminNav";
